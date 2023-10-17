be8aae439734c3b8e8c26908fd3390c6836cbfc6
gayaz83 <gokhanayaz1983@gmail.com>
2023-10-17 12:43:24 +0200

    exercise2

--- DERS TEKRAR/SAYAC/index.js
+++ DERS TEKRAR/SAYAC/index.js
@@ -1,17 +1,27 @@
+// let count = 0
+// let saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-el")
+
+// function increment() {
+//     count += 1
+//     countEl.textContent = count
+// }
+
+// function save() {
+//     let countStr = count + " - "
+//     saveEl.textContenttStr
+//     countEl.textContent = 0
+//     count = 0
+// }
+
+// console.log("Let's count sheeps on the field!")
+
+
+let countEl = document.getElementById("count-el") 
+
 let count = 0
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
 
-function increment() {
+function increment () {
     count += 1
-    countEl.textContent = count
+    countEl.innerText = count
 }
-
-function save() {
-    let countStr = count + " - "
-    saveEl.textContenttStr
-    countEl.textContent = 0
-    count = 0
-}
-
-console.log("Let's count sheeps on the field!")
--- JS/workshop/workshop.js
+++ JS/workshop/workshop.js
@@ -1,59 +1,59 @@
-console.log("slm")
+// console.log("slm")
 
 
-// let myAge1 = 36
-// console.log(myAge);
+// // let myAge1 = 36
+// // console.log(myAge);
 
-let sayı1 = 5*7
-console.log(sayı1);
+// let sayı1 = 5*7
+// console.log(sayı1);
 
-let say2 = sayı1
-console.log(say2);
+// let say2 = sayı1
+// console.log(say2);
 
-let myAge = 35
-let dog = myAge*7
-console.log(dog);
-
-
-let bonus = 50
-console.log(bonus);
-
-bonus = bonus + 50
-console.log(bonus);
-bonus = bonus - 75
-console.log(bonus);
-
-function increment () {
-    console.log("The button was clicked");
-} 
-
-increment()
-
-console.log(5);
-console.log(4);
-console.log(3);
-console.log(2);
-console.log(1);
-console.log("start");
-
-function countdown () {
-    console.log(5);
-    console.log(4);
-    console.log(3);
-    console.log(2);
-    console.log(1);
-    console.log("start");
-}
-
-countdown ()
-countdown ()
+// let myAge = 35
+// let dog = myAge*7
+// console.log(dog);
 
 
-function sayac () {
-    console.log(42);
-}
+// let bonus = 50
+// console.log(bonus);
 
-sayac()
+// bonus = bonus + 50
+// console.log(bonus);
+// bonus = bonus - 75
+// console.log(bonus);
+
+// function increment () {
+//     console.log("The button was clicked");
+// } 
+
+// increment()
+
+// console.log(5);
+// console.log(4);
+// console.log(3);
+// console.log(2);
+// console.log(1);
+// console.log("start");
+
+// function countdown () {
+//     console.log(5);
+//     console.log(4);
+//     console.log(3);
+//     console.log(2);
+//     console.log(1);
+//     console.log("start");
+// }
+
+// countdown ()
+// countdown ()
+
+
+// function sayac () {
+//     console.log(42);
+// }
+
+// sayac()
 
 
 let lap1 = 34
@@ -61,6 +61,27 @@ let lap2 = 33
 let lap3 = 36
 
 function topla () {
-    return topla = lap1+lap2+lap3
-    console.log(topla);
+    let toplam = lap1+lap2+lap3
+    console.log(toplam);
 }
+
+topla ()
+
+let sayac = 0
+ function arttir () {
+    sayac += 1
+    console.log(sayac);
+ }
+
+arttir ()
+
+let userName = "Gokhan"
+let message = "You have 3 notifications"
+
+let messageToUsername = message + " " + userName + "!"
+
+console.log(messageToUsername);
+
+let store = document.getElementById("parag")
+
+store.innerText = message + username
