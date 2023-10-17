b2da369b7b51afb9222bcf499f89f02bed499d43
gayaz83 <gokhanayaz1983@gmail.com>
2023-10-17 09:58:48 +0200

    exercise1

--- JS/workshop/workshop.js
+++ JS/workshop/workshop.js
@@ -1,5 +1,66 @@
-console.log("workshop tan gelen not")
+console.log("slm")
 
-let x=5
-let y=3
-console.log(x+y)
+
+// let myAge1 = 36
+// console.log(myAge);
+
+let sayı1 = 5*7
+console.log(sayı1);
+
+let say2 = sayı1
+console.log(say2);
+
+let myAge = 35
+let dog = myAge*7
+console.log(dog);
+
+
+let bonus = 50
+console.log(bonus);
+
+bonus = bonus + 50
+console.log(bonus);
+bonus = bonus - 75
+console.log(bonus);
+
+function increment () {
+    console.log("The button was clicked");
+} 
+
+increment()
+
+console.log(5);
+console.log(4);
+console.log(3);
+console.log(2);
+console.log(1);
+console.log("start");
+
+function countdown () {
+    console.log(5);
+    console.log(4);
+    console.log(3);
+    console.log(2);
+    console.log(1);
+    console.log("start");
+}
+
+countdown ()
+countdown ()
+
+
+function sayac () {
+    console.log(42);
+}
+
+sayac()
+
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function topla () {
+    return topla = lap1+lap2+lap3
+    console.log(topla);
+}
